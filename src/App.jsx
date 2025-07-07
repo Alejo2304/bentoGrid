2e5e97f15ed5b3b854ba708dbeaac25af8308652
Alejo2304 <ojeda.alejandro.2304@gmail.com>
2025-07-07 13:38:29 -0500

    BentoGrid.jsx | App.jsx | Containers.jsx
    Defined a responsive h from lg screen to 80vh on BentoGrid.jsx, defined the total height of main on app.jsx to h-screen, update font size to match the design on containers.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import BentoGrid from "./components/BentoGrid.jsx"
 
 function App() {
   return (
-    <main className="bg-whitebg flex justify-center items-center">
+    <main className="bg-whitebg flex justify-center items-center h-screen">
         <BentoGrid/>
     </main>
   );
--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -1,7 +1,7 @@
 export function Yellow500Container1({ text, imgPath, imgAlt }) {
     return (
         <div className="bg-yellow-500 px-4 pt-4 rounded-lg h-65  overflow-hidden shadow-lg space-y-5 md:h-full md:w-full">
-            <h1 className="font-dmsans font-medium text-3xl">
+            <h1 className="font-dmsans font-medium text-3xl md:text-4xl">
                 {text}
             </h1>
             <img src={imgPath} alt={imgAlt} className="w-2/3  md:h-[110%] md:w-auto" />
@@ -12,7 +12,7 @@ export function Yellow500Container1({ text, imgPath, imgAlt }) {
 export function Yellow500Container2({ text, imgPath, imgAlt }) {
     return (
         <div className="flex flex-col md:items-center md:justify-center bg-yellow-500  rounded-lg shadow-lg p-5 space-y-5 md:h-full md:w-full">
-            <h1 className="font-dmsans font-medium text-4xl">
+            <h1 className="font-dmsans font-medium text-4xl md:text-5xl">
                 {text}
             </h1>
             <img src={imgPath} alt={imgAlt} className="w-[15rem]"/>
@@ -23,7 +23,7 @@ export function Yellow500Container2({ text, imgPath, imgAlt }) {
 export function Yellow100Container({ text, emText, imgPath, imgAlt }) {
     return (
         <div className="flex flex-col md:items-center md:justify-center bg-yellow-100 rounded-lg shadow-lg p-5 space-y-5 md:h-full  md:w-full">
-            <h1 className="font-dmsans font-medium text-4xl">
+            <h1 className="font-dmsans font-medium text-4xl md:text-5xl">
                 {text} <em className="italic text-purple-500">{emText}</em>
             </h1>
             <img className="w-[15rem]" src={imgPath} alt={imgAlt} />
@@ -33,16 +33,16 @@ export function Yellow100Container({ text, emText, imgPath, imgAlt }) {
 
 export function WhiteContainer({ text, imgPath, imgAlt }) {
     return (
-        <div className="bg-white rounded-lg shadow-lg p-5 md:overflow-hidden md:h-full md:w-full">
+        <div className="bg-white rounded-lg shadow-lg p-5 space-y-5 md:overflow-hidden md:h-full md:w-full">
             <img className="drop-shadow-md rounded-[1lh] md:min-w-[20em]" src={imgPath} alt={imgAlt} />
-            <h1 className="font-dmsans font-medium text-2xl pr-4">{text}</h1>
+            <h1 className="font-dmsans font-medium text-2xl md:text-4xl pr-4">{text}</h1>
         </div>
     );
 }
 
 export function WhiteContainerDescriptive({ textTitle, textDescription, imgPath, imgAlt }) {
     return (
-        <div className="bg-white p-6 space-y-3 rounded-lg shadow-lg md:h-full md:w-full">
+        <div className="bg-white p-6 space-y-5 rounded-lg shadow-lg md:h-full md:w-full">
             <h1 className="font-dmsans font-medium text-6xl">{textTitle}</h1>
             <p className="font-dmsans font-medium text-xl">{textDescription}</p>
             <img className="w-1/2 md:w-auto md:h-[3lh]"src={imgPath} alt={imgAlt} />
@@ -73,9 +73,9 @@ export function PurpleContainerDescriptive({ startTitle, emTitle, italicTitle, e
 
 export function PurpleContainer({ text, imgPath, imgAlt }) {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center bg-purple-500 rounded-lg shadow-lg p-5 space-y-8 md:h-full md:w-full">
-            <img className="flex-1/2 md:flex-1/3 w-[20rem] md:h-[90%] md:w-auto"src={imgPath} alt={imgAlt} />
-            <h1 className="flex-1/2 md:flex-2/3 font-dmsans font-medium text-3xl md:text-4xl text-white text-center">{text}</h1>
+        <div className="flex flex-col md:flex-row items-center justify-center bg-purple-500 rounded-lg shadow-lg p-5 space-y-4 space-x-3 md:h-full md:w-full">
+            <img className="flex-1/2 md:flex-1/3 w-[15rem] md:h-[90%] md:w-auto"src={imgPath} alt={imgAlt} />
+            <h1 className="flex-1/2 md:flex-2/3 font-dmsans font-medium text-3xl md:text-4xl md:text-start text-white text-center">{text}</h1>
         </div>
     );
 }
@@ -83,9 +83,9 @@ export function PurpleContainer({ text, imgPath, imgAlt }) {
 export function Purple100Container({ textTitle, textDescription, imgPath, imgAlt }) {
     return (
         <div className=" md:overflow-hidden flex flex-col items-center justify-center bg-purple-100 rounded-lg shadow-lg space-y-5 px-5 py-6 md:h-full md:w-full">
-            <h1 className="font-dmsans font-medium text-center text-3xl">{textTitle}</h1>
+            <h1 className="font-dmsans font-medium text-center md:text-start text-3xl md:text-4xl">{textTitle}</h1>
             <img className='w-20rem' src={imgPath} alt={imgAlt} />
-            <p className="font-dmsans font-normal text-xl text-center px-5 ">{textDescription}</p>
+            <p className="font-dmsans font-normal text-xl text-center md:text-start px-5 ">{textDescription}</p>
         </div>
     );
 }
